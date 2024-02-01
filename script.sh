2dd3fc7e259be9d359e09524cf3f6c62a795998f
Matias Vinkovic <73315516+MatiasVinkovic@users.noreply.github.com>
2024-02-01 20:19:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,7 +30,7 @@ if [ -s "file" ];then	#if file is not empty : rm all its content but not itself
 	rm -r -f -d file/*
 fi
 
-if [ -s "images" ];then	#if file is not empty : rm all its content but not itself
+if [ -s "images" ];then	#if images is not empty : rm all its content but not itself
 	#echo "temp file exists : rm its content"
 	rm -r -f -d images/*
 fi
@@ -50,6 +50,13 @@ if [[ ! $1 == *.csv ]];then
 	echo "is not a .csv file"
 	exit 9
 fi
+
+
+# Couleurs
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'  # No Color
 
 dollar_at=$@ #all the args of the line
 
@@ -75,14 +82,17 @@ gnuplot << EOF
 		set style fill solid 1.0 border 
 		set style histogram rowstacked
 		set xtics rotate
-		set y2tics rotate
-		set y2range [0:250]
+
+		set autoscale y
+		set yrange [0:*]
+
+		set ytics rotate
 		set datafile separator ";"
 		plot for [COL=1:2] "temp/d1_data.data" using COL:xticlabel(2) lc rgb "blue"
 EOF
 	END=$(date +%s)
 	DIFF=$(( $END - $START ))
-	echo "It took $DIFF seconds"
+	echo -e "${YELLOW}It took $DIFF seconds${NC}"
 	#sudo apt install 	imagemagick : to manipulate images in shell
 	convert -rotate 90 images/d1_output.png images/d1_output.png
 	#display images/d1_output.png
@@ -90,7 +100,7 @@ EOF
 
 	-d2)
 	START=$(date +%s)
-		LC_NUMERIC=en_US.UTF-8 awk -F';' '{distance[$6] += $5} END {for (name in distance) print distance[name],";"name }' data.csv | sort -k1rn | head -n 10 > temp/d2_data.data
+		LC_NUMERIC=en_US.UTF-8 awk -F';' '{distance[$6] += $5} END {for (name in distance) print distance[name],";"name }' $1 | sort -k1rn | head -n 10 > temp/d2_data.data
 gnuplot << EOF
 		reset
 		set terminal pngcairo size 800,1200 enhanced font "arial,12"
@@ -99,13 +109,17 @@ gnuplot << EOF
 		set boxwidth 0.75
 		set xlabel "Driver name" rotate by 180
 		set y2label "Distances (in km)"
-		set y2range [0:160000]
-		set y2tics rotate
+		
+		set ytics rotate
 		set ylabel "option -d2 : Distance = f(Driver)"
 		set style data histogram
 		set style fill solid 1.0 border 
 		set style histogram rowstacked
 		set xtics rotate
+
+		set autoscale y
+		set yrange [0:*]
+		
 		set datafile separator ";"
 		plot for [COL=1:1] "temp/d2_data.data" using COL:xticlabel(2) lc rgb "#308c1f" title "" 
 EOF
@@ -118,10 +132,10 @@ EOF
 
 	-l)		
 		START=$(date +%s)																																									 #put r to have same as the teacher				
-		LC_NUMERIC=en_US.UTF-8 awk -F';' '{distance[$1] += $5} END {for (routeID in distance) print distance[routeID],";"routeID}' data.csv | sort -k1n | tail -n 10 | sort -t';' -k2n > temp/l_data.data
+		LC_NUMERIC=en_US.UTF-8 awk -F';' '{distance[$1] += $5} END {for (routeID in distance) print distance[routeID],";"routeID}' $1 | sort -k1n | tail -n 10 | sort -t';' -k2n > temp/l_data.data
 		END=$(date +%s)
 		DIFF=$(( $END - $START ))
-		echo "It took $DIFF seconds"
+		echo -e "${YELLOW}It took $DIFF seconds${NC}"
 gnuplot << EOF
 		reset
 		set terminal pngcairo size 800,800 enhanced font "arial,12"
@@ -133,21 +147,27 @@ gnuplot << EOF
 		set style fill solid 1.0 border
 		set style histogram rowstacked
 		set datafile separator ";"
-		set yrange [0:3000]
-		set ytics 0,500,3000
+		set ytics rotate
+		set autoscale y
+		
+		set yrange [0:*]
 		plot "temp/l_data.data" using 1:xticlabels(2) lc rgb "#308c1f" with boxes title ""
 EOF
 	#display images/d2_output.png
 		
 		;;
 	-s)	
-		cat data.csv | cut -d';' -f1,5 | tail -n +2 | sed 's/;/ /g' > file/option_s_data.txt
+		START=$(date +%s)
+		cat $1 | cut -d';' -f1,5 | tail -n +2 | sed 's/;/ /g' > file/option_s_data.txt
 		
-		echo "T treatment launching ..."
+		echo "S treatment launching ..."
 		cd progC/option_s/
 		echo "MakeFile Initialisation in process..."
-		make
-		echo "MakeFile succesfully finished"
+		echo -e "${RED}"
+		if [ ! -e "prog" ];then
+			make
+		fi;
+		echo -e "${GREEN}MakeFile succesfully finished${NC}"
 		./prog
 		cd ../../
 		cd file/
@@ -158,8 +178,8 @@ EOF
 		nl -s';' -w1 -nrz option_s.data > s_data.data #put the line number : CANT DO IT WITH INFIX AND COUNT WTF
 		rm option_s.data
 
-		#il faut encore modifier le "oui.data"
-		#ainsi que gerer le fichier gnuplot et essayer de separer les scripts gnuplot comme fait
+		
+		#And also manage the Gnuplot file and attempt to separate the Gnuplot scripts as done.		
 		cd ..
 		cd gnuplot_file/
 		chmod u+r+w+x option_s_gnuplot.sh
@@ -170,16 +190,24 @@ EOF
 		cd progC/option_s/
 		make clean
 		cd ../../
+		END=$(date +%s)
+		DIFF=$(( $END - $START ))
+		echo -e "${YELLOW}It took $DIFF seconds${NC}"
 															 			
 	;;
 
 	-t)	
-		cat data.csv | sed 's/ /-/g' | sed 's/;/ /g' | cut -d' ' -f1-4 | tail -n +2 > file/option_t_data.txt
+		START=$(date +%s)
+		cat $1 | sed 's/ /-/g' | sed 's/;/ /g' | cut -d' ' -f1-4 | tail -n +2 > file/option_t_data.txt
 		
 		echo "T treatment launching ..."
 		cd progC/option_t/
 		echo "MakeFile Initialisation in process..."
-		make
+		echo -e "${RED}"
+		if [ ! -e "prog" ];then
+			make
+		fi;
+		echo -e "${GREEN}"
 		echo "Make file succesfully finished"
 		./prog
 		cd ../../
@@ -197,12 +225,15 @@ EOF
 		cd progC/option_t/
 		make clean
 		cd ../../
+		END=$(date +%s)
+		DIFF=$(( $END - $START ))
+		echo -e "${YELLOW}It took $DIFF seconds${NC}"
 		
 													 			
 	;;
 
 
 
-		*) echo "$arg n'existe pas";;
+		*) echo -e "${RED}$arg n'existe pas${NC}";;
 	esac
 done
